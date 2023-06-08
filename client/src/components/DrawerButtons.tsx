1e1cd077647546cd66c55ec207ce95c8a88f20b0
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-06-09 03:30:47 +0530

    Landing page

---
+++ client/src/components/DrawerButtons.tsx
@@ -0,0 +1,11 @@
+function DrawerButtons({ Name }: { Name?: string }) {
+  return (
+    <div className="flex justify-center p-2">
+      <button className="bg-primary-light duration-200 text-primary rounded-md max-w-[20rem] w-full py-2 hover:scale-105 transition-[transform]">
+        {Name}
+      </button>
+    </div>
+  );
+}
+
+export default DrawerButtons;
