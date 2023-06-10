3827949f1f3d8d694ff16537bea61136c9173ecf
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-06-10 22:14:30 +0530

    Login Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import LandingPage from './components/LandingPage'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 // import './App.css'
 
 function App() {
-
   return (
-    <>
-      <LandingPage/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+
+  );
 }
 
-export default App
+export default App;
