b8f8f32f3a585e7319bf5b610c396a9b468fb6d4
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-06-16 21:09:16 +0530

    fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 // import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <div className="w-screen h-screen flex bg-slate-950"></div>
+  )
+}
+
+export default Home
